5f0254c49c156c78c9775ebbbbce19ccc54d9619
Jandro <jandro@jandro.localdomain>
2024-12-03 22:08:36 -0400

    letters and characters allowed

--- wordMaster/wordMasters.js
+++ wordMaster/wordMasters.js
@@ -1,37 +1,41 @@
 const WORD_URL = "https://words.dev-apis.com/word-of-the-day";
 const VALIDATE_URL = "/validate-word";
-let boxIdName = "box-";
+const boxName = "box-";
 let i = 1;
 
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 };
 
+function gameLogic() {
+  document.addEventListener("keydown", function(event) {
+    if (isLetter(event.key)) {
+      document
+        .getElementById(boxName + i)
+        .innerText = event.key;
+      i++;
+    } else if (event.key == "Backspace") {
+      if (i > 1) {
+        i--;
+      }
+      document
+        .getElementById(boxName + i)
+        .innerText = "";
+    } else {
+      event.preventDefault();
+    }
+  });
+}
+
 async function init() {
 
   const promise = await fetch(WORD_URL);
   const wordObject = await promise.json();
-  let wordAnswer = wordObject.word;
-  // console.log(wordAnswer);
-  console.log(boxIdName + i);
-  document
-    .getElementById(boxIdName + i)
-    .addEventListener("keydown", function(event) {
-      if (!isLetter(event.key) && event.key != "Backspace") {
-        event.preventDefault();
-      }
-      if (isLetter(event.key)) {
-        i++;
-        console.log(i)
-      } else {
-        if (i > 1) {
-          i--;
-        }
-        console.log(i)
-      }
-    })
+  // let wordAnswer = wordObject.word;
 
-  // async function validWord() {
+  gameLogic();
+
+  // async function validateWord() {
   //   const promise = await fetch(WORD_URL + VALIDATE_URL, {
   //     method: "POST",
   //     body: JSON.stringify({ "word": "hello" })
