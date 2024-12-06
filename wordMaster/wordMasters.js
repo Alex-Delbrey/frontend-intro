58769c2fa91085c3da13de3ccbb514a0cde8d992
Jandro <jandro@jandro.localdomain>
2024-12-05 22:36:08 -0400

    knocked a couple of things out the todo

--- wordMaster/wordMasters.js
+++ wordMaster/wordMasters.js
@@ -9,26 +9,50 @@ function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 };
 
+function backspace() {
+  if (i > 1) {
+    i--;
+  }
+  document
+    .getElementById(boxName + i)
+    .innerText = "";
+  wordGuess = wordGuess.substring(0, wordGuess.length - 1);
+}
+
+async function validateWord(wordToValidate) {
+  const promise = await fetch(VALIDATE_URL, {
+    method: "post",
+    body: JSON.stringify({ "word": wordToValidate })
+  });
+  const validatedWord = await promise.json();
+  return validatedWord.validWord;
+}
+
 // TODO:
 // - CANNOT backspace after valid attempt
-// - line 28 is stepping inside the if without waiting for response from condition
-// - make sure wordGuess is a valid word POST method
-// -> if valid word: 
-// --> paint green if letter is in correct place
-// --> paint yellow if letter is part of answer but incorrect spot
 // - compare wordGuess to wordAnswer
-// - allow user to backspace when in the last letter
 // - change alert message
 // - officialize the end of the game when user wins 
 // OR when user runs out of guesses
+// -> if valid word: 
+// --> paint green if letter is in correct place
+// --> paint yellow if letter is part of answer but incorrect spot
 function gameLogic() {
   document.addEventListener("keydown", function(event) {
     if (endOfWord) {
       if (event.key === "Enter") {
-        if (validateWord(wordGuess)) {
-          wordGuess = "";
-          endOfWord = false;
-        }
+        validateWord(wordGuess).then(
+          (isValid) => {
+            if (isValid) {
+              wordGuess = "";
+              endOfWord = false;
+            } else {
+              alert("Word is not a word, ya herd?");
+            }
+          });
+      } else if (event.key === "Backspace") {
+        backspace();
+        endOfWord = false;
       }
       if (i === 31) {
         alert("GAME ENDED, GET OUTTA HERE");
@@ -43,27 +67,11 @@ function gameLogic() {
         .innerText = event.key;
       i++;
     } else if (event.key === "Backspace") {
-      if (i > 1) {
-        i--;
-      }
-      document
-        .getElementById(boxName + i)
-        .innerText = "";
-      wordGuess = wordGuess.substring(0, wordGuess.length - 1);
+      backspace();
     } else {
       event.preventDefault();
     }
   });
-}
-
-async function validateWord(wordToValidate) {
-  const promise = await fetch(VALIDATE_URL, {
-    method: "post",
-    body: JSON.stringify({ "word": wordToValidate })
-  });
-  const validatedWord = await promise.json();
-  console.log(validatedWord.validWord);
-  return validatedWord.validWord;
 }
 
 async function init() {
